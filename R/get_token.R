8f754b5d5db949a87a82bf8abe270faaf2c9f24d
Hiren Joshi <hirenj@gmail.com>
2017-09-27 15:12:54 +0200

    Basic package running

--- R/get_token.R
+++ R/get_token.R
@@ -12,8 +12,8 @@ gatordata_endpoint <- httr::oauth_endpoint(
 
 get_session_id = function() {
   json_body = jsonlite::toJSON(list(
-    client_id=keyring::key_get('gatordata.client_id'),
-    client_secret=keyring::key_get('gatordata.client_secret'),
+    client_id=keyring::key_get('gatordata.client_id',server_endpoint),
+    client_secret=keyring::key_get('gatordata.client_secret',server_endpoint),
     audience=server_config$API_AUDIENCE,
     grant_type='client_credentials'
   ),auto_unbox = T)
@@ -22,20 +22,18 @@ get_session_id = function() {
 
   token_data = httr::POST(gatordata_endpoint$access,body=json_body, httr::add_headers("Content-Type" = "application/json"))
 
-  # 4. Use API
   url <- httr::modify_url(
     url = server_endpoint,
     path = c("api", "login")
   )
 
-  req <- POST(url,
+  req <- httr::POST(url,
               httr::add_headers(
-                "Authorization" = paste('Bearer',content(token_data)$access_token),
+                "Authorization" = paste('Bearer',httr::content(token_data)$access_token),
                 "x-api-key" = keyring::key_get('gatordata.client_id')
               ))
 
-  #stop_for_status(req)
-  session_id = content(req)$session_id
+  session_id = httr::content(req)$session_id
   return (session_id)
 }
 install_packages = function() {
@@ -46,7 +44,16 @@ list_packages = function() {
   session_id = get_session_id()
   available.packages(httr::modify_url(url=server_endpoint, path=c('api','repository','token',session_id,'src','contrib')))
 }
+list_package_urls = function() {
+  files = list_packages()[,c('File','Repository')]
+  paste(files[,2],files[,1],sep='/')
+}
 update_packages = function() {
   session_id = get_session_id()
   update.packages(repos=httr::modify_url(url=server_endpoint, path=c('api','repository','token',session_id)))
+}
+
+auto_update = function() {
+  initialize()
+  update_packages()
 }
--- R/init.R
+++ R/init.R
@@ -1,1 +1,16 @@
 #init
+
+server_endpoint = ifelse('gatordata.server' %in% names(options()), getOption('gatordata.server'), 'https://glycodomain.glycomics.ku.dk')
+
+initialize = function() {
+  stored_client_ids = keyring::key_list('gatordata.client_id')$username
+  stored_client_secrets = keyring::key_list('gatordata.client_secret')$username
+  if (! server_endpoint %in% stored_client_ids) {
+    message("There is no Client ID set for ",server_endpoint," please add one now")
+    keyring::key_set('gatordata.client_id',server_endpoint)
+  }
+  if (! server_endpoint %in% stored_client_secrets) {
+    message("There is no Client secret set for ",server_endpoint," please add one now")
+    keyring::key_set('gatordata.client_secret',server_endpoint)
+  }
+}
